0d5fd6bdf85099c87c220ee90b656cbfa6ef3a74
shubham79a <shubh79.nolan@gmail.com>
2025-05-10 13:16:44 +0530

    project setup and shadcn i

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// later
+import { Inter, Space_Grotesk } from "next/font/google";
+const inter = Inter({ subsets: ['latin'],
+  variable: '--font-inter',
+ })
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-space-grotesk',
+  weight: ['300', '400', '500', '600', '700']
+})
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pricewise",
+  description: "Track product prices effortlessly and save money on your online shopping.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${spaceGrotesk.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
